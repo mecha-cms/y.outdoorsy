c5dcc626d3c26e558fe50bbe5a9db889494683d0
Taufik Nurrohman <hi@taufik-nurrohman.com>
2023-04-30 19:51:15 +0700

    Use Built-In Search Form Component

--- widget/form/search.php
+++ widget/form/search.php
@@ -1,22 +1,8 @@
 <?php
 
-$content = "";
-
-if (isset($state->x->search)) {
-    $to = $url . ($route ?? $state->routeBlog);
-    $value = $_GET[$key = $state->x->search->key ?? 'query'] ?? null;
-    $content .= '<form action="' . eat($to) . '" method="get" name="search" role="search">';
-    $content .= '<p>';
-    $content .= '<input name="' . eat($key) . '" type="text"' . (null !== $value ? ' value="' . eat($value) . '"' : "") . '>';
-    $content .= ' ';
-    $content .= '<button type="submit">' . i('Search') . '</button>';
-    $content .= '</p>';
-    $content .= '</form>';
-} else {
-    $content .= '<p role="status">' . i('Missing %s extension.', ['<a href="https://mecha-cms.com/store/extension/search" target="_blank">search</a>']) . '</p>';
-}
-
 echo self::widget([
-    'content' => $content,
+    'content' => isset($state->x->search) ? self::form('search', [
+        'route' => $route ?? $state->routeBlog,
+    ]) : '<p role="status">' . i('Missing %s extension.', ['<a href="https://mecha-cms.com/store/extension/search" target="_blank">search</a>']) . '</p>',
     'title' => $title ?? i('Search')
 ]);
