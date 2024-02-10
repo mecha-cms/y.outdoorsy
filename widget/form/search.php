22d6b653f96a2a93161f3b4df9b79b90f88d0bc1
Taufik Nurrohman <hi@taufik-nurrohman.com>
2024-02-10 12:27:26 +0700

    HTML `<search>`

--- widget/form/search.php
+++ widget/form/search.php
@@ -1,8 +1,8 @@
 <?php
 
 echo self::widget([
-    'content' => isset($state->x->search) ? self::form('search', [
+    'content' => isset($state->x->search) ? '<search>' . self::form('search', [
         'route' => $route ?? $state->routeBlog ?? '/article',
-    ]) : '<p role="status">' . i('Missing %s extension.', ['<a href="https://mecha-cms.com/store/extension/search" target="_blank">search</a>']) . '</p>',
+    ]) . '</search>' : '<p role="status">' . i('Missing %s extension.', ['<a href="https://mecha-cms.com/store/extension/search" target="_blank">search</a>']) . '</p>',
     'title' => $title ?? i('Search')
 ]);
